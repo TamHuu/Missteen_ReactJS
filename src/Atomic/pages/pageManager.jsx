0d5f6aa1e98fa7049788eae44e95d84708d4b325
HuuTam <christopherle0902@gmail.com>
2022-07-01 10:58:07 +0700

    missteen

--- src/Atomic/pages/pageManager.jsx
+++ src/Atomic/pages/pageManager.jsx
@@ -20,7 +20,6 @@ import DialogEdit from "./Form/formEdit";
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
-    
   },
 });
 
@@ -31,14 +30,13 @@ export default function DenseTable() {
   const [dataDialog, setDataDialog] = useState({});
   const [open, setOpen] = useState(false);
 
- 
   // view
-  const [edit,setEdit]=useState(false)
-const[dataEdit,setDataEdit]=useState({})
+  const [edit, setEdit] = useState(false);
+  const [dataEdit, setDataEdit] = useState({});
 
   const handleChange = (item) => {
     setOpen(true);
-    setUser(item)
+    setUser(item);
   };
   const handleClose = () => {
     setOpen(false);
@@ -47,120 +45,121 @@ const[dataEdit,setDataEdit]=useState({})
     const idRow = DataTable.findIndex((item) => item.id === id);
     setDataDialog(DataTable[idRow]);
   };
-console.log(user)
-// Sua
-const editHandleClose=()=>{
- setEdit(false)
+  console.log(user);
+  // Sua
+  const editHandleClose = () => {
+    setEdit(false);
+  };
 
-}
-
-
-
-const handleChangeEdit = (item)=>{
-  setDataEdit(item)
-  setEdit(true)
-}
+  const handleChangeEdit = (item) => {
+    setDataEdit(item);
+    setEdit(true);
+  };
 
   return (
     <>
- {edit&&<DialogEdit
-                    dataEdit={dataEdit}
-                    onClose={editHandleClose}
+      {edit && (
+        <DialogEdit
+          dataEdit={dataEdit}
+          onClose={editHandleClose}
+          title={"Chinh sua"}
+        />
+      )}
 
-                    title={'Chinh sua'}
-                  />}
-               
+      {open && (
+        <DialogMissTeen
+          onClose={handleClose}
+          onView={ViewHandler}
+          user={user}
+          title={"Quản lý thí sinh"}
+        />
+      )}
 
-               
-
-
-      {open&&<DialogMissTeen
-                    onClose={handleClose}
-                    onView={ViewHandler}
-                    user={user}
-                    title={'Quản lý thí sinh'}
-                  />}
-                
-    <TableContainer  component={Paper} style={{ padding: "100px" }}>
-     
-      <Table className={classes.table} size="small" aria-label="a dense table"  >
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ fontSize: "20px" }}>STT</TableCell>
-            <TableCell style={{ fontSize: "20px" }} align="left">
-              Tên thí sinh
-            </TableCell>
-            <TableCell style={{ fontSize: "20px" }} align="center">
-              Hình ảnh
-            </TableCell>
-         
-            <TableCell style={{ fontSize: "20px" }} align="left">
-              Năm Sinh
-            </TableCell>
-            <TableCell style={{ fontSize: "20px" }} align="left">
-              Chiều cao
-            </TableCell>
-            <TableCell style={{ fontSize: "20px" }} align="left">
-              Cân Nặng
-            </TableCell>
-            <TableCell style={{ fontSize: "20px" }} align="left">
-              Công việc
-            </TableCell>
-            <TableCell style={{ fontSize: "20px" }} align="center">
-              Thao tác
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {lists.map((list) => (
-            <TableRow    >
-              <TableCell component="th" scope="row" >
-                {list.id}
+      <TableContainer component={Paper} style={{ margin: " 57px, 0" }}>
+        <Table
+          className={classes.table}
+          size="small"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ fontSize: "20px" }}>STT</TableCell>
+              <TableCell style={{ fontSize: "20px" }} align="left">
+                Tên thí sinh
               </TableCell>
-              <TableCell align="left">{list.name}</TableCell>
-              <TableCell align="center">
-                <Avatar src={list.img} variant="square"></Avatar>
+              <TableCell style={{ fontSize: "20px" }} align="center">
+                Hình ảnh
               </TableCell>
-              {/* <TableCell align="left">{list.age}</TableCell> */}
-              <TableCell align="left">{list.born}</TableCell>
-              <TableCell align="left">{list.height}</TableCell>
-              <TableCell align="left">{list.weight}</TableCell>
-              <TableCell align="left">{list.jobs}</TableCell>
-              <TableCell align="center" >
-                {/* Xem */}
-                <IconButton
-                  variant="contained"
-                  color="primary"
-                  style={{ margin: 5 }}
-                  onClick={() =>{ ViewHandler(list.id); handleChange(list)}}
-       
-                >
-                 
-                  <Visibility />
-                </IconButton>                      
-               
-                <IconButton
-                  variant="contained"
-                  color="primary"
-                  style={{ margin: 5 }}
-                  onClick={()=>{handleChangeEdit(list)}}
-                >
-                  <EditIcon />
-                </IconButton>                      
-                <IconButton
-                  variant="contained"
-                  color="primary"
-                  style={{ margin: 5 }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-               
+
+              <TableCell style={{ fontSize: "20px" }} align="left">
+                Năm Sinh
+              </TableCell>
+              <TableCell style={{ fontSize: "20px" }} align="left">
+                Chiều cao
+              </TableCell>
+              <TableCell style={{ fontSize: "20px" }} align="left">
+                Cân Nặng
+              </TableCell>
+              <TableCell style={{ fontSize: "20px" }} align="left">
+                Công việc
+              </TableCell>
+              <TableCell style={{ fontSize: "20px" }} align="center">
+                Thao tác
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {lists.map((list) => (
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  {list.id}
+                </TableCell>
+                <TableCell align="left">{list.name}</TableCell>
+                <TableCell align="center">
+                  <Avatar src={list.img} variant="square"></Avatar>
+                </TableCell>
+                {/* <TableCell align="left">{list.age}</TableCell> */}
+                <TableCell align="left">{list.born}</TableCell>
+                <TableCell align="left">{list.height}</TableCell>
+                <TableCell align="left">{list.weight}</TableCell>
+                <TableCell align="left">{list.jobs}</TableCell>
+                <TableCell align="center">
+                  {/* Xem */}
+                  <IconButton
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: 5 }}
+                    onClick={() => {
+                      ViewHandler(list.id);
+                      handleChange(list);
+                    }}
+                  >
+                    <Visibility />
+                  </IconButton>
+
+                  <IconButton
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: 5 }}
+                    onClick={() => {
+                      handleChangeEdit(list);
+                    }}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: 5 }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
